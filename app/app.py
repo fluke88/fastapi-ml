03b1b206a76c0e2891cd9371b26da64737073ce2
fluke8 <frist410@gmail.com>
2024-05-24 15:33:43 +0700

    123

--- app/app.py
+++ app/app.py
@@ -1,38 +1,58 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from dataclasses import dataclass
+from typing import List
 
 from ml.model import load_staff
 
 model = None
 app = FastAPI()
 
+class PredictParams(BaseModel):
+    datetime: str
+    geo_lat: float
+    geo_lon: float
+    building_type: int
+    level: int
+    levels: int
+    rooms: int
+    area: float
+    kitchen_area: float
+    object_type: int
+    studio_apartment: int
+    region: str
 
-@dataclass
-class CostPrediction:
-    score: float
+    class Config:
+        schema_extra = {
+            "example": {
+                "datetime": "2021-08-01",
+                "geo_lat": 59.805808,
+                "geo_lon": 30.376141,
+                "building_type": 1,
+                "level": 1,
+                "levels": 8,
+                "rooms": 3,
+                "area": 82.6,
+                "kitchen_area": 10.8,
+                "object_type": 1,
+                "studio_apartment": 1,
+                "region": "Санкт-Петербург"
+            }
+        }
 
-
-# create a route
 @app.get("/")
 def index():
     return {"params": "Cost"}
 
-
-# Register the function to run during startup
 @app.on_event("startup")
 def startup_event():
     global model
     model = load_staff()
 
-
-# Your FastAPI route handlers go here
-@app.get("/predict")
-def predict_cost(params: list):
-    cost = model(params)
-    print(cost)
-    response = CostPrediction(
-        score=cost.score,
-    )
+@app.post("/predict")
+def predict_cost(params: PredictParams):
+    model_prediction = model(params.dict())
+    
+    response = model_prediction
 
     return response
